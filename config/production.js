ac9b7f63f9b99fa5f5caa795fbffaefc00642ba9
shubham5526 <41041596+shubham5526@users.noreply.github.com>
2021-09-28 19:25:00 +0530

    Added production js

---
+++ config/production.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+module.exports = {
+    CLIENT_ID: '3MVG9p1Q1BCe9GmDaW6g0.88BooCsSpJicA0r7qp.BDZfWLPHQ1On.PWlSyD1NZzARRTOMV_Madtv_A_qS5bn',
+    FIREBASE_PROJECT_ID: '<YOUR FIREBASE PROJECT ID>',
+    GOOGLE_ANALYTICS_ID: ''
+};
